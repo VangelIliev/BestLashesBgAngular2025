338d56b3c605ce27b3f6c837faf7c681e55991d8
Vangel Iliev <Vangel_Iliev@outlook.com>
2025-10-25 18:12:35 +0300

    import the components into the app router

--- bestlashesbgangular.client/src/app/app-routing.module.ts
+++ bestlashesbgangular.client/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeImageCardComponent } from './home-image-card/home-image-card.component';
+import { LashesComponent } from './lashes/lashes.component';
+import { ShoppingBasketComponent } from './shopping-basket/shopping-basket.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { ThankYouPageComponent } from './thank-you-page/thank-you-page.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeImageCardComponent },
+  { path: 'lashes', component: LashesComponent },
+  { path: 'shopping-basket', component: ShoppingBasketComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'reviews', component: ReviewsComponent },
+  { path: 'thank-you', component: ThankYouPageComponent },
+  { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
